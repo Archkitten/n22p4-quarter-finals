4cd6442219332eea42bdffb37b61892ef0aa187f
RohanG326 <gaikwadrohan326@gmail.com>
2022-06-01 12:56:48 -0700

    delete database

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from sportslocalapi.sportapi import sportapi_bp
 from tennis.tennis import tennis_pg
 from cruddy.app_notes import app_notes
 from cruddy.app_content import app_content
-from workout.app_workout import app_workout
-from workout.app_workout_api import app_workout_api
-import requests
 
 app.register_blueprint(sportapi_bp)
 app.register_blueprint(app_crud)
@@ -17,8 +14,6 @@ app.register_blueprint(app_crud_api)
 app.register_blueprint(tennis_pg)
 app.register_blueprint(app_notes)
 app.register_blueprint(app_content)
-app.register_blueprint(app_workout)
-app.register_blueprint(app_workout_api)
 
 @app.route('/')
 def index():
@@ -41,10 +36,6 @@ def physicaltherapy():
 @app.route('/scoreboard/')
 def scoreboard():
     return render_template("scoreboard.html")
-
-@app.route('/teamapi/')
-def teamapi():
-    return render_template("teamapi.html")
 
 @app.route('/workouts/')
 def workouts():
